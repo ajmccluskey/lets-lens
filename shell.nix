be12bab7c2a6af916fbace84892f8864802d3e94
Andrew McCluskey <andrew@ajmccluskey.com>
2019-01-31 11:32:59 +1000

    Add doctest to shell.nix.

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@
 let
   inherit (nixpkgs) pkgs;
   drv = import ./default.nix { inherit nixpkgs compiler; };
-  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [ pkgs.cabal-install ];
+  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [ pkgs.cabal-install pkgs.haskellPackages.doctest ];
 in
   if pkgs.lib.inNixShell then drvWithTools.env else drvWithTools
